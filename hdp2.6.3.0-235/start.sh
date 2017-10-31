66d93389a6fcc3b3ef2ba7c26a1d409077625447
Dongjoon Hyun <dongjoon@apache.org>
2017-10-31 12:49:15 -0700

    Add HDP 2.6.3.0

--- hdp2.6.3.0-235/start.sh
+++ hdp2.6.3.0-235/start.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,22 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-VERSION=${1%/}
-IMAGE=dongjoon/${VERSION:-hdp2.5}
+/usr/sbin/sshd
 
-LINK=""
-for i in {1..3}
-do
-    HOST=hdn-001-0$i
-    LINK="$LINK --link=$HOST:$HOST"
-    docker run --name=$HOST -h $HOST -p 1001$i:22 -d $IMAGE /root/start.sh
-done
-
-HOST=hnn-001-01
-PORT="-p 4040:4040 -p 5050:5050 -p 8088:8088 -p 10000:10000 -p 10010:22 -p 26002:26002 -p 26080:26080 -p 50070:50070"
-docker run --name=$HOST -h $HOST $PORT $LINK -it --rm -v $SPARK_HOME:/spark $IMAGE /root/init-nn.sh
-
-for i in {1..3}
-do
-    docker rm -f hdn-001-0$i
-done
+while true; do sleep 1000; done
